13e2c0241b574fd00938a8312d00e61c937674e8
SJ <vince15dk@gmail.com>
2019-01-31 16:43:56 +0900

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
-            
+
+function scrollToBottom(){
+    // Selectors
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child')
+
+    // Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+       messages.scrollTop(scrollHeight);
+    }
+}
 
 socket.on('connect', function (){
     console.log('Connected with server');
@@ -19,7 +35,7 @@ socket.on('newMessage', function(message){
     });
 
     jQuery('#messages').append(html);
-   
+    scrollToBottom();
     // let li = jQuery('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`);
     // jQuery('#messages').append(li);
@@ -59,7 +75,7 @@ socket.on('newLocationMessage', function(message){
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My current location</a>');  
 
