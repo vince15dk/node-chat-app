243f1b2c269c7b114c7c553d312df426a163f618
SJ <vince15dk@gmail.com>
2019-01-31 12:13:03 +0900

    Add jQuery to Chat rook

--- public/js/index.js
+++ public/js/index.js
@@ -11,17 +11,31 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage', function(message){
     console.log('newMessage', message);
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
 })
 
 socket.on('newJoin', function(message){
     console.log(message);
 })
 
-socket.emit('createMessage', {
-    from: 'Frank',
-    text: 'Hi'
-}, function(message){
-    console.log(message);
+// socket.emit('createMessage', {
+//     from: 'Frank',
+//     text: 'Hi'
+// }, function(message){
+//     console.log(message);
+// });
+
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function(){
+        
+    })
 });
 
 
